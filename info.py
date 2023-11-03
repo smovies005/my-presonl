e1e6a9e58abeae072f8f13b52d6ed2c8ce3da6e3
smovies005 <148788708+smovies005@users.noreply.github.com>
2023-11-03 12:46:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
-PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
+PICS = (environ.get('PICS' ,'https://telegra.ph/file/4a956c1ae043c1bbfd714.jpg https://telegra.ph/file/8eef7261831e296b2fcab.jpg https://telegra.ph/file/7c7d22be7c24e388572ee.jpg https://telegra.ph/file/90e55bb5dc99a4182ec4d.jpg https://telegra.ph/file/00496c018793bbef81268.jpg https://telegra.ph/file/1917d8a1756bfd0852d8b.jpg https://telegra.ph/file/80c0665135169062ca9a3.jpg https://telegra.ph/file/a7d9fbf2dd7d794b6995b.jpg https://telegra.ph/file/fd78ec524e66edb51f6bf.jpg https://telegra.ph/file/2699ed36a05e26bf7cab0.jpg')).split()
 UPTIME = time.time()
 
 # Admins, Channels & Users
